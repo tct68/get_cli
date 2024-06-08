7bd796efa3af9fa2c3d7a4ee045d00ac25e66dd5
TCT <thuong.truong@enouvo.com>
2024-06-08 11:19:54 +0700

    Upgrade

--- lib/samples/impl/get_app_pages.dart
+++ lib/samples/impl/get_app_pages.dart
@@ -4,12 +4,9 @@ import '../interface/sample_interface.dart';
 /// [Sample] file from [app_pages] file creation.
 class AppPagesSample extends Sample {
   String initial;
-  AppPagesSample(
-      {String path = 'lib/app/routes/app_pages.dart', this.initial = 'HOME'})
-      : super(path);
+  AppPagesSample({String path = 'lib/app/routes/app_pages.dart', this.initial = 'home'}) : super(path);
   final import = PubspecUtils.getPackageImport;
-  String get _initialRoute =>
-      initial.isNotEmpty ? '\nstatic const INITIAL = Routes.$initial;' : '';
+  String get _initialRoute => initial.isNotEmpty ? '\nstatic const initial = Routes.$initial;' : '';
 
   @override
   String get content => '''$import
--- lib/samples/impl/get_route.dart
+++ lib/samples/impl/get_route.dart
@@ -5,9 +5,8 @@ class RouteSample extends Sample {
   RouteSample({String path = 'lib/app/routes/app_routes.dart'}) : super(path);
   @override
   String get content => '''part of 'app_pages.dart';
-// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
 
-abstract class Routes{
+abstract class Routes {
   Routes._();
 
 }
